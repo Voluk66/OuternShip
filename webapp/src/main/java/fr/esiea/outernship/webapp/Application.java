128117f9fe7284ee6a8868ae9611b2f31b7b4711
Maxime Maitrot <maitrot@et.esiea.fr>
2022-09-23 17:37:37 +0200

    Ajout du webapp

--- webapp/src/main/java/fr/esiea/outernship/webapp/Application.java
+++ webapp/src/main/java/fr/esiea/outernship/webapp/Application.java
@@ -1,14 +1,11 @@
 package fr.esiea.outernship.webapp;
 
-
-import fr.esiea.outernship.webapp.Controller.ApplicationController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(ApplicationController.class);
-        springApplication.run(args);
+    public static void main(String[] args){
+        SpringApplication.run(Application.class);
     }
 }
---
+++ webapp/src/main/java/fr/esiea/outernship/webapp/controller/ApplicationController.java
@@ -0,0 +1,13 @@
+package fr.esiea.outernship.webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ApplicationController {
+    @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
+    public String index() {
+        return "home";
+    }
+}
