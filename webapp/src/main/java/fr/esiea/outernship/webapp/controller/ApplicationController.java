c17e5a7e7659f3e812bd9a12129ffec82cbd206a
Maxime Maitrot <maitrot@et.esiea.fr>
2023-01-23 01:31:34 +0100

    Update

--- webapp/src/main/java/fr/esiea/outernship/webapp/controller/ApplicationController.java
+++ webapp/src/main/java/fr/esiea/outernship/webapp/controller/ApplicationController.java
@@ -3,12 +3,51 @@ package fr.esiea.outernship.webapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class ApplicationController {
     @RequestMapping(value = {"/", "/home"}, method = RequestMethod.GET)
-    public String index() {
-        return "home";
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("home");
+        return mv;
+    }
+
+    @RequestMapping(value = "/planification", method = RequestMethod.GET)
+    public ModelAndView planification(){
+        ModelAndView mv = new ModelAndView("planification");
+        return mv;
+    }
+
+    @RequestMapping(value = "/analyse", method = RequestMethod.GET)
+    public ModelAndView analyse(){
+        ModelAndView mv = new ModelAndView("analyse");
+        return mv;
+    }
+
+    @RequestMapping(value = "/ideation", method = RequestMethod.GET)
+    public ModelAndView ideation(){
+        ModelAndView mv = new ModelAndView("ideation");
+        return mv;
+    }
+
+    @RequestMapping(value = "/conception", method = RequestMethod.GET)
+    public ModelAndView conception(){
+        ModelAndView mv = new ModelAndView("conception");
+        return mv;
+    }
+
+    @RequestMapping(value = "/testUtilisateur", method = RequestMethod.GET)
+    public ModelAndView testUtilisateur(){
+        ModelAndView mv = new ModelAndView("testUtilisateur");
+        return mv;
+    }
+
+    @RequestMapping(value = "/maquette",method = RequestMethod.GET)
+    public ModelAndView maquette(){
+        ModelAndView mv = new ModelAndView("maquette");
+        return mv;
     }
 
 }
